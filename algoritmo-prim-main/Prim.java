b5bc5a9cdd0e25ff849e88204323c8455abc61c3
Juan Carlos Sanchez <77439369+Juanki019@users.noreply.github.com>
2023-05-21 17:20:27 +0200

    Update Prim.java

--- algoritmo-prim-main/Prim.java
+++ algoritmo-prim-main/Prim.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
 
 public class Prim {
 
@@ -26,13 +24,13 @@ public class Prim {
     public static void main(String[] args) {
         //No se que hay que poner en info vertice hay que preguntar
         Grafo<String, String, Integer> grafo = new Grafo<>();
-        grafo.insertarVertice("A", "B");
-        grafo.insertarVertice("B", "C");
-        grafo.insertarVertice("C", "C");
-        grafo.insertarVertice("D", "C");
-        grafo.insertarVertice("E", "C");
-        grafo.insertarVertice("F", "C");
-        grafo.insertarVertice("G", "C");
+        grafo.insertarVertice("A", null);
+        grafo.insertarVertice("B", null);
+        grafo.insertarVertice("C", null);
+        grafo.insertarVertice("D", null);
+        grafo.insertarVertice("E", null);
+        grafo.insertarVertice("F", null);
+        grafo.insertarVertice("G", null);
         grafo.insertarArista("A", "B", 10);
         grafo.insertarArista("A", "D", 7);
         grafo.insertarArista("A", "C", 4);
@@ -48,13 +46,6 @@ public class Prim {
         int peso = algoritmoPrim(grafo, aristas);
         escribirLista(aristas);
         System.out.println("Peso total: " + peso);
-
-
-
-
-
-
-
 
 
         //Agregamos los vertices y aristas y despues sacamos el peso
